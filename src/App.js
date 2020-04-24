d6ba637c8da6d7588b2f967d2f545cdb89c74587
StephenAraka <stevenaraka@gmail.com>
2020-04-24 21:20:32 +0300

    import components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+// import styles from './App.module.css';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        App 
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
