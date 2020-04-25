85d3742844c1b1aabf03b3a232176c236995f426
StephenAraka <stevenaraka@gmail.com>
2020-04-25 14:24:08 +0300

    Updates

---
+++ precache-manifest.4f534601c7a2a4cb9226a011fd3fb02a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3999b3de7b3e514b70eb7671dbbdb3",
+    "url": "/covid-tracker-app/index.html"
+  },
+  {
+    "revision": "1f44ab9dd768d24eaf10",
+    "url": "/covid-tracker-app/static/css/main.9c385ae8.chunk.css"
+  },
+  {
+    "revision": "0304685a27e920d62d71",
+    "url": "/covid-tracker-app/static/js/2.cf8d1451.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-app/static/js/2.cf8d1451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f44ab9dd768d24eaf10",
+    "url": "/covid-tracker-app/static/js/main.0eb3f89b.chunk.js"
+  },
+  {
+    "revision": "687bc383eb8d2eccf1bc",
+    "url": "/covid-tracker-app/static/js/runtime-main.1f706314.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-app/static/media/image.d7265326.png"
+  }
+]);
